5009af817df145dc5185818f3178bd5262c71dac
David Conley <David@Davids-MacBook-Pro.local>
2016-04-04 15:29:00 -0400

    add currency_test.py

---
+++ currency_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+from currencty import *
+
+class TestPalindrome(unittest.TestCase):
+
+    def test_even_numbers(self):
+        self.assertTrue(is_palindrome('toot'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
