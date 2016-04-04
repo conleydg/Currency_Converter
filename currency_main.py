5a94ec89f3f6730fe232cade13ce3373bc1e9a52
David Conley <David@Davids-MacBook-Pro.local>
2016-04-04 15:31:53 -0400

    adding files

---
+++ currency_main.py
@@ -0,0 +1,12 @@
+from currency import *
+from currency_converter import *
+
+test_currency = Currency(50, '$')
+
+
+
+test_currency_two = Currency(50, '$')
+
+print(test_currency == test_currency_two)
+
+# test_currency.add_currency(test_currency_two)
