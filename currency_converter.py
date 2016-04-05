2a3e81a591a7b1af4afc2351ee62d68869695eb4
David Conley <David@Davids-MacBook-Pro.local>
2016-04-04 20:25:17 -0400

    now returns updated  original currency instance

--- currency_converter.py
+++ currency_converter.py
@@ -1,6 +1,5 @@
-# CurrencyConverter objects
-# Must be initialized with a dictionary of currency codes to conversion rates (see link to rates below).
-# At first, just make this work with two currency codes and conversation rates, with one rate being 1.0 and the other being the conversation rate. An example would be this: {'USD': 1.0, 'EUR': 0.74}, which implies that a dollar is worth 0.74 euros.
+
+
 # Must be able to take a Currency object and a requested currency code that is the same currency code as the Currency object's and return a Currency object equal to the one passed in. That is, currency_converter.convert(Currency(1, 'USD'), 'USD') == Currency(1, 'USD').
 # Must be able to take a Currency object that has one currency code it knows and a requested currency code and return a new Currency object with the right amount in the new currency code.
 # Must be able to be created with a dictionary of three or more currency codes and conversion rates. An example would be this: {'USD': 1.0, 'EUR': 0.74, 'JPY': 120.0}, which implies that a dollar is worth 0.74 euros and that a dollar is worth 120 yen, but also that a euro is worth 120/0.74 = 162.2 yen.
@@ -9,23 +8,20 @@
 
 conversion_rates = {'USD': 1.0, 'EUR': 0.74, 'JPY': 120.0}
 
-class CurrencyConverter:
+class CurrencyConverter():
     def __init__(self, conversion_rates):
         self.conversion_rates = conversion_rates
 
     def convert(self, currency_instance, new_currency):
+        print(currency_instance.amount, currency_instance.currency_code)
         to_dollars = currency_instance.amount / (self.conversion_rates[currency_instance.currency_code])
-        print(to_dollars)
-        convert_to_new_currency = to_dollars * self.conversion_rates[new_currency]
-        print("%.2f" % convert_to_new_currency, new_currency)
+        new_ammount = to_dollars * self.conversion_rates[new_currency]
+        # currency_instance = Currency("%.2f" % new_ammount, new_currency)
+        currency_instance.amount = ("%.2f" % new_ammount)
+        currency_instance.currency_code = new_currency
+        return(currency_instance.amount, currency_instance.currency_code)
 
+        # currency_instance = Currency("%.2f" % convert_to_new_currency, new_currency)
+        # return currency_instance
 
-
-        # if currency_instace.currency_code == 'USD':
-        #     return rate_to_USD = 1
-        #
-        # elif currency_instace.currency_code == 'Eur':
-        #     rate_to_USD = currency_instance.amount / .74
-        #
-        # elif currency_instace.currency_code == 'JPY':
-        #     rate_to_USD = currency_instance.amount / 120
+        # ("%.2f" % convert_to_new_currency, new_currency)
