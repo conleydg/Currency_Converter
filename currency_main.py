3d6b37e5aa3fc8bec6a10e12dcca955c426f195d
David Conley <David@Davids-MacBook-Pro.local>
2016-04-04 21:58:26 -0400

    test are in here

--- currency_main.py
+++ currency_main.py
@@ -1,12 +1,35 @@
 from currency import *
 from currency_converter import *
 
-test_currency = Currency(50, '$')
 
 
+test_currency = Currency(5000, 'JPY')
+test_currency_two = Currency(5000, 'JPY')
+test_currency_three = Currency(5000, 'USD')
 
-test_currency_two = Currency(50, '$')
+print('test_currency: ',test_currency.amount, test_currency.currency_code)
+print('test_currency_two : ',test_currency_two.amount, test_currency_two.currency_code)
+print('test_currency_three : ',test_currency_three.amount, test_currency_three.currency_code)
 
-print(test_currency == test_currency_two)
+conversion_rates = {'USD': 1.0, 'EUR': 0.74, 'JPY': 120.0}
+
+converter_one = CurrencyConverter(conversion_rates)
+
+print('test_currency == test_currency_two: ', test_currency == test_currency_two)
+print('test_currency != test_currency_three: ', test_currency != test_currency_three)
+
+print('test_currency + test_currency_two: ', test_currency + test_currency_two)
+
+print('test_currency - test_currency_two: ',test_currency - test_currency_two)
+#
+print('test_currency * 3', test_currency * 3)
+
+#
+converter_one.convert(test_currency, 'USD')
+
+print('test_currency converted from JPY to USD: )
+print(test_currency.amount, test_currency.currency_code)
+#
+
 
 # test_currency.add_currency(test_currency_two)
